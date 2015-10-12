1bdc28b6b4df6f864dfd0c23103772681682b39d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-10-12 13:51:27 +0200

    Implemented Nurse.dependency_manager

--- lib/nurse.rb
+++ lib/nurse.rb
@@ -2,4 +2,7 @@ require "nurse/version"
 require "nurse/dependency_container"
 
 module Nurse
+  def self.dependency_manager
+    @@dependency_manager ||= DependencyContainer.new
+  end
 end
--- spec/nurse_spec.rb
+++ spec/nurse_spec.rb
@@ -4,4 +4,12 @@ describe Nurse do
   it "has a version" do
     refute_nil ::Nurse::VERSION
   end
+
+  describe ".dependency_manager" do
+    it "returns a singleton instance of DependencyContainer" do
+      container = Nurse.dependency_manager
+      container.must_be_instance_of(Nurse::DependencyContainer)
+      Nurse.dependency_manager.must_be_same_as(container)
+    end
+  end
 end
